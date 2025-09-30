907711c3d8d17595349cfb212b05fef91bbc7b4f
marmora193 <mar_193@hotmail.com>
2025-10-01 00:46:35 +0200

    Se crea un tab control con las pestañas Datos Personales, Preferencias y Visualización.

--- GestionAvanzadaDeInformacionPersonal/Form1.Designer.cs
+++ GestionAvanzadaDeInformacionPersonal/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace GestionAvanzadaDeInformacionPersonal
 {
-    partial class Form1
+    partial class FormPrincipal
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -28,13 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tcGestion = new System.Windows.Forms.TabControl();
+            this.tpDatosPersonales = new System.Windows.Forms.TabPage();
+            this.tpPreferencias = new System.Windows.Forms.TabPage();
+            this.tpVisualizacion = new System.Windows.Forms.TabPage();
+            this.tcGestion.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tcGestion
+            // 
+            this.tcGestion.Controls.Add(this.tpDatosPersonales);
+            this.tcGestion.Controls.Add(this.tpPreferencias);
+            this.tcGestion.Controls.Add(this.tpVisualizacion);
+            this.tcGestion.Location = new System.Drawing.Point(-7, 12);
+            this.tcGestion.Name = "tcGestion";
+            this.tcGestion.SelectedIndex = 0;
+            this.tcGestion.Size = new System.Drawing.Size(812, 447);
+            this.tcGestion.TabIndex = 0;
+            // 
+            // tpDatosPersonales
+            // 
+            this.tpDatosPersonales.Location = new System.Drawing.Point(4, 25);
+            this.tpDatosPersonales.Name = "tpDatosPersonales";
+            this.tpDatosPersonales.Padding = new System.Windows.Forms.Padding(3);
+            this.tpDatosPersonales.Size = new System.Drawing.Size(804, 418);
+            this.tpDatosPersonales.TabIndex = 0;
+            this.tpDatosPersonales.Text = "Datos Personales";
+            this.tpDatosPersonales.UseVisualStyleBackColor = true;
+            // 
+            // tpPreferencias
+            // 
+            this.tpPreferencias.Location = new System.Drawing.Point(4, 25);
+            this.tpPreferencias.Name = "tpPreferencias";
+            this.tpPreferencias.Padding = new System.Windows.Forms.Padding(3);
+            this.tpPreferencias.Size = new System.Drawing.Size(398, 112);
+            this.tpPreferencias.TabIndex = 1;
+            this.tpPreferencias.Text = "Preferencias";
+            this.tpPreferencias.UseVisualStyleBackColor = true;
+            // 
+            // tpVisualizacion
+            // 
+            this.tpVisualizacion.Location = new System.Drawing.Point(4, 25);
+            this.tpVisualizacion.Name = "tpVisualizacion";
+            this.tpVisualizacion.Padding = new System.Windows.Forms.Padding(3);
+            this.tpVisualizacion.Size = new System.Drawing.Size(398, 112);
+            this.tpVisualizacion.TabIndex = 2;
+            this.tpVisualizacion.Text = "Visualización";
+            this.tpVisualizacion.UseVisualStyleBackColor = true;
+            // 
+            // FormPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.tcGestion);
+            this.Name = "FormPrincipal";
+            this.Text = "Gestión Avanzada de Información personal";
+            this.tcGestion.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tcGestion;
+        private System.Windows.Forms.TabPage tpDatosPersonales;
+        private System.Windows.Forms.TabPage tpPreferencias;
+        private System.Windows.Forms.TabPage tpVisualizacion;
     }
 }
 
